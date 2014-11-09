815e4479448372f3d7d4c7eb30a327cbb7dafd36
Hector Calderon <Hector@Hectors-MacBook-Pro-2.local>
2014-11-08 20:48:31 -0700

    test cases

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,24 +1,37 @@
-# require 'test_helper'
+require 'test_helper'
 
-# class UsersControllerTest < Test::Unit::TestCase
-#  #  fixtures :users
-#  #  def setup
-#  #    @controller = UserController.new
-#  #    @request    = ActionController::TestRequest.new
-#  #    @response   = ActionController::TestResponse.new
-#  #  end
+class UsersControllerTest < ActionController::TestCase
+ test "should create user" do
+	  assert_difference('User.count') do
+	    post :create, user: {
+	    	name: 'tester',
+	    	password: 'password',
+	    	password_dup: 'password',
+	    	email: 'tester@gmail.com',
+	    	email_dup: 'tester@gmail.com'
+	    }
+	  end
+	 
+	  assert_redirected_to root_url
+	end
 
-#  #  def test_create_user
-#  #    get :search, :title => 'Ruby Tutorial'
-#  #    assert_not_nil assigns(:user)
-#  #    assert_equal books(:perl_cb).title, assigns(:book).title
-#  #    assert_valid assigns(:book)
-#  #    assert_redirected_to :action => 'show'
-#  #  end
+	test "should get user profile page if signed in" do
+		post :create, user: {
+	    	name: 'tester',
+	    	password: 'password',
+	    	password_dup: 'password',
+	    	email: 'tester@gmail.com',
+	    	email_dup: 'tester@gmail.com'
+	    }
 
-#  # def test_search_not_found
-#  #    get :search, :title => 'HTML Tutorial'
-#  #    assert_redirected_to :action => 'list'
-#  #    assert_equal 'No such book available', flash[:error]
-#  #  end
-# end
+    get :show, {id: 1}
+    assert_response :success
+    assert_template "users/show"
+	end
+
+	test "should redirect to sign up page if not signed in" do
+		session[:token] = nil
+		get :show, {id: 1}
+		assert_redirected_to "users/new"
+	end
+end
--- test/models/friendship_test.rb
+++ test/models/friendship_test.rb
@@ -7,10 +7,4 @@ class FriendshipTest < ActiveSupport::TestCase
 			friendship.save!
 	  end
 	end
-
-	test "cannot create same friendship twice" do
-		friendship = Friendship.create(id: 1, user_id: 1, friend_id: 2)
-		friendship2 = Friendship.create(id: 2, user_id: 1, friend_id: 2)
-		assert_equal "has already been taken", friendship2.errors[:friend_id][0]
-	end
 end
